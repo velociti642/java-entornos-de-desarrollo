b11717e82f564f7f4b2938d18679bb942b58acb4
velociti642 <131817459+velociti642@users.noreply.github.com>
2024-03-19 18:42:34 +0100

    Clase c

---
+++ Figuras/src/modelos/Circulo.java
@@ -0,0 +1,50 @@
+package modelos;
+
+public class Circulo {
+
+    private double radio;
+    
+    /**
+     * Default constructor
+     */
+    public Circulo() {
+    	this.radio = 1;
+    }
+    /**
+     * @param radio
+     */
+    public void Circulo(double radio) {
+        this.radio = radio;
+    }
+
+    /**
+     * @return
+     */
+    public double getRadio() {
+        return this.radio;
+    }
+
+    /**
+     * @return
+     */
+    public double getPerimetro() {
+        // TODO implement here
+        return Math.PI * (this.radio * 2);
+    }
+
+    /**
+     * @return
+     */
+    public double getArea() {
+        // TODO implement here
+        return Math.PI * (this.radio * this.radio);
+    }
+
+    /**
+     * @param radio
+     */
+    public void setRadio(int radio) {
+        // TODO implement here
+    }
+
+}
---
+++ Figuras/src/modelos/Triangulo.java
@@ -0,0 +1,59 @@
+package modelos;
+
+public class Triangulo {
+
+
+
+    private double base;
+    private double altura;
+
+    public Triangulo() {
+    	this.base = 1;
+    	this.altura = 1;
+    }
+    
+    /**
+     * @param base 
+     * @param altura
+     */
+    public void Triangulo(double base, double altura) {
+        this.altura = altura;
+        this.base = base;
+    }
+
+    /**
+     * @return
+     */
+    public double getBase() {
+        return this.base;
+    }
+
+    /**
+     * @return
+     */
+    public double getAltura() {
+        return this.altura;
+    }
+
+    /**
+     * @return
+     */
+    public double getArea() {
+        return (this.base * this.altura) / 2;
+    }
+
+    /**
+     * @param base
+     */
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    /**
+     * @param altura
+     */
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+}
