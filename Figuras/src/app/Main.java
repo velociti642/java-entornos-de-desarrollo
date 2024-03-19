8ab3f45827109ff36ce34e062e4261fb94e53768
velociti642 <131817459+velociti642@users.noreply.github.com>
2024-03-19 16:32:27 +0100

    Main implementado

--- Figuras/src/app/Main.java
+++ Figuras/src/app/Main.java
@@ -1,11 +1,20 @@
 package app;
 
-import java.util.Scanner;
+import modelos.Rectangulo;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.print("Introduzca el valor de la base: ");
+		
+		Rectangulo r1 = new Rectangulo();
+		Rectangulo r2 = new Rectangulo();
+
+		int area1 = r1.getArea();
+		int perimetro = r2.getPerimetro();
+		
+		System.out.print("El área del rectangulo es: " + area1 + "\n El perímetro del rectangulo es: " + perimetro);
+		
+		
 	}
 
 }
